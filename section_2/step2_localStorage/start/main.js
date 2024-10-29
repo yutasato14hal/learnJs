9b770bfb36be0268e3744d65f3a87dd1b57a127f
yutasato14hal <83742609+yutasato14hal@users.noreply.github.com>
2024-10-29 19:01:28 +0900

    commit

--- section_2/step2_localStorage/start/main.js
+++ section_2/step2_localStorage/start/main.js
@@ -2,11 +2,32 @@ const todoInput = document.getElementById('todoInput');
 const addBtn = document.getElementById('addTodo');
 const todoList = document.getElementById('todoList');
 
-addBtn.addEventListener('click',function(){
-  const newTodo = todoInput.value.trim();
+// const savedTodos = JSON.stringify(newTodo)
+//   localStorage.setItem('todo', savedTodos);
+
+function addTodoList() {
   const li = document.createElement('li');
   li.innerHTML = newTodo;
   todoList.appendChild(li);
+}  const Btn = document.createElement('button');
+function addDeleteBtn() {
 
-  
+  Btn.innerHTML = "削除";
+  li.appendChild(Btn);
+}
+addBtn.addEventListener('click', function () {
+  const newTodo = todoInput.value.trim();
+  addTodoList()
+  todoInput.value = "";
 })
+Btn.addEventListener('click', function () {
+  // DOMから削除
+  li.remove();
+  // localstrageから削除
+
+})
+
+
+// li.addEventListener("click", function comp() {
+//   li.classList.toggle('completed');
+// })
